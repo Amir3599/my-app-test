53c962284b2fcc73ce8b7d287d8ec923745e0e28
Amir3599 <amir.a.koochaki@gmail.com>
2024-02-21 21:31:31 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ChatBot from "my-test-chatbot-package";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatBot position="bottom-left" greetingMessage="Greet" />
     </div>
   );
 }
